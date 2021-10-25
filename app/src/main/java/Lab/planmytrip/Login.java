103ab5daa9c58a7e9c4569a427f2003c08c3cc4b
Mark Socaciu <61942320+somark2000@users.noreply.github.com>
2021-10-25 18:12:03 +0300

    L2-partial

--- app/src/main/java/Lab/planmytrip/Login.java
+++ app/src/main/java/Lab/planmytrip/Login.java
@@ -2,15 +2,12 @@ package Lab.planmytrip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.auth.api.credentials.Credentials;
 
 public class Login extends AppCompatActivity {
 
@@ -26,7 +23,7 @@ public class Login extends AppCompatActivity {
         ed1 = (EditText)findViewById(R.id.uname);
         ed2 = (EditText)findViewById(R.id.passw);
 
-        b2 = (Button)findViewById(R.id.register);
+        b2 = (Button)findViewById(R.id.register_goto);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,8 +37,13 @@ public class Login extends AppCompatActivity {
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                openRegister();
             }
         });
+    }
+
+    private void openRegister(){
+        Intent intent=new Intent(this,Register.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/Lab/planmytrip/Register.java
+++ app/src/main/java/Lab/planmytrip/Register.java
@@ -1,13 +1,15 @@
 package Lab.planmytrip;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
-    Button b1;//register
+    Button b1,b2;//register
     EditText ed1,ed2;//fname,lname,uname, passw,repassw,mail
 
 
@@ -15,5 +17,17 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePickerDialog(view);
+            }
+        });
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
