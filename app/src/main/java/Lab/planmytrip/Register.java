9dc29db10bfd9f172ddaf5f682f748bf78cea705
Mark Socaciu <61942320+somark2000@users.noreply.github.com>
2021-10-25 18:42:07 +0300

    L2-partial

--- app/src/main/java/Lab/planmytrip/Register.java
+++ app/src/main/java/Lab/planmytrip/Register.java
@@ -3,31 +3,44 @@ package Lab.planmytrip;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
-public class Register extends AppCompatActivity {
-    Button b1,b2;//register
-    EditText ed1,ed2;//fname,lname,uname, passw,repassw,mail
+import java.util.Calendar;
 
+public class Register extends AppCompatActivity {
+    Button b1;
+    EditText fname,lname,uname,passw1,passw2,mail,date;//fname,lname,uname, passw,repassw,mail,date
+    DatePickerDialog picker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        b2.setOnClickListener(new View.OnClickListener() {
+        date=(EditText)findViewById(R.id.bdate);
+        date.setInputType(InputType.TYPE_NULL);
+        date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showDatePickerDialog(view);
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(Register.this,new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                date.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
             }
         });
-    }
-
-    public void showDatePickerDialog(View v) {
-        DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
