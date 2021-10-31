e82e504b6e678776682f991a03de6722c03b1130
Șoaita M. Monica <61684336+monix8400@users.noreply.github.com>
2021-10-31 17:15:34 +0200

    get some data from database for homepage +fixed User default constructor

--- app/src/main/java/Lab/planmytrip/Home.java
+++ app/src/main/java/Lab/planmytrip/Home.java
@@ -1,5 +1,6 @@
 package Lab.planmytrip;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -8,13 +9,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Home extends AppCompatActivity {
 
     private TextView title;
     private Button btn;
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+
+    private String userID;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -22,14 +35,32 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        user= FirebaseAuth.getInstance().getCurrentUser();
+        reference= FirebaseDatabase.getInstance().getReference("Users");
+        userID= user.getUid();
+
         title=(TextView) findViewById(R.id.textView);
         btn=(Button) findViewById(R.id.button);
 
-        title.setText("Hello "+"@User");
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile= snapshot.getValue(User.class);
+
+                if(userProfile!=null){
+                    String fullName= userProfile.fname+" "+userProfile.lname;
+                    title.setText("Hello\n"+fullName+"!");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(Home.this,"Something wrong happened!",Toast.LENGTH_LONG).show();
+            }
+        });
+
         btn.setText("LOGOUT");
-
         btn=(Button) findViewById(R.id.button);
-
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,5 +69,4 @@ public class Home extends AppCompatActivity {
             }
         });
     }
-
 }
--- app/src/main/java/Lab/planmytrip/User.java
+++ app/src/main/java/Lab/planmytrip/User.java
@@ -4,6 +4,8 @@ public class User {
 
     public String fname,lname,passw,email,date,phone;
 
+    public User(){}
+
     public User(String fname, String lname, String passw, String email, String date,String phone) {
         this.fname = fname;
         this.lname = lname;
